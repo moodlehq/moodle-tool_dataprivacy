a6b6dffdd6e84960ae504b3fd6ec5ab13fab70f8
David Monllao <david.monllao@gmail.com>
2018-04-26 15:42:34 +0200

    NOBUG: Bump to 33.5.0

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017051509;
+$plugin->version   = 2017051510;
 $plugin->requires  = 2017051505.04;        // Moodle 3.3.5+ (Build: 20180412) and upwards.
 $plugin->component = 'tool_dataprivacy';
-$plugin->release   = '33.4.0';
+$plugin->release   = '33.5.0';
 $plugin->maturity  = MATURITY_BETA;
